3eaf2f0cc3c18b491aa9550a40cac259a0228d2a
Francisco Ciordia <gatupancho@gmail.com>
2025-03-31 14:58:15 -0300

    fix wip: wrong icons

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,5 @@
 export const CONNECTED_WALLET_ICON = '/img/icons/connectedWallet.svg#icon';
 export const DISCONNECTED_WALLET_ICON = '/img/icons/disconnectedWallet.svg#icon';
+export const RELOAD_ICON = '/img/icons/reload.svg#icon';
+
 export const ADA = '/img/icons/ADA.svg#icon';
