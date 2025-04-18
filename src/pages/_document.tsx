39815081dc23ae1fd113202fe2a5a3e93c131165
Francisco Ciordia <gatupancho@gmail.com>
2025-04-18 16:58:15 -0300

    doc: add icon and page name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export const metadata = {
     title: 'MAYZ - OTC',
-    description: '',
+    description: 'OTC service of MAYZ protocol',
     applicationName: 'MAYZ - OTC',
     themeColor: '#ffffff',
 };
@@ -14,8 +14,14 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <meta name="description" content={metadata.description} />
-                    <meta name="application-name" content={metadata.applicationName} />
-                    <meta name="theme-color" content={metadata.themeColor} />
+                <meta name="application-name" content={metadata.applicationName} />
+                <meta name="theme-color" content={metadata.themeColor} />
+                <link rel="icon" type='image/ico' href="icon.ico" />
+                {/* <link rel="manifest" href="/site.webmanifest" /> */}
+                {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f0a500" /> */}
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             </Head>
             <body>
                 <Main />
